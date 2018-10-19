6ecf55366395e3bf7affda1955d73d10fb1667e8
Marc Eastman <eastmanmarcusc@gmail.com>
2018-10-19 12:47:38 -0400

    enabled font-awesome and bootstrap componenets

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 class App extends Component {
   render() {
@@ -11,6 +13,12 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          //bootstrap alert element
+          <div class="alert alert-danger" role="alert">
+            <span class="sr-only">Error:</span>
+            Enter a valid email address
+          </div>
+          <FontAwesomeIcon icon={faCoffee} />
           <a
             className="App-link"
             href="https://reactjs.org"
